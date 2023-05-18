ebc108e0b951c7421bd65e13c1276c0781327c29
Anton <45099803+MrAntonS@users.noreply.github.com>
2023-05-17 21:26:54 -0400

    Went through vectors in udemy, boriiiiing

--- Section1/src/main.cpp
+++ Section1/src/main.cpp
@@ -1,18 +1,24 @@
 #include <iostream>
+#include <stdexcept>
+#include <limits>
 
-using namespace std;
+ using namespace std;
 
-int main() {
+ int main() {
     int fav_num;
     while (true){
         cout << "Pick a number between 1 and 100" << endl;
         cin >> fav_num;
+        if (cin.fail()) {
+          cin.clear();
+          cin.ignore(numeric_limits<streamsize>::max(),'\n');
+        }
         if (fav_num > 100 || fav_num < 1) {
             cout << "No the number should be in 1 to 100 range!!!!" << endl;
         } else {
-            break;
-        }
-    }
+        break;
+       } 
+       }
     cout << "You won't believe me, but it's my favorite number as well, " << fav_num << "!!!" << endl;
-    return 0;
-}
+   return 0;
+ }
---
+++ StructureOfC++/src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << 1.0 / 2.2345 << endl;
+    return 0;
+}
